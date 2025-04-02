fb21fad27514c25c3f83c5f2af9042de5b1e29cc
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-04-02 19:28:10 +0530

    Route: Add "Homepage" for pre/post authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,18 @@ Rails.application.routes.draw do
   devise_for(:users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" })
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  devise_scope(:user) do
+    authenticated(:user) do
+      root(to: "posts#index", as: :authenticated_homepage)
+    end
+
+    unauthenticated(:user) do
+      root(to: "devise/sessions#new")
+    end
+  end
+
+  resources(:posts)
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get("up" => "rails/health#show", :as => :rails_health_check)
