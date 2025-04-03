8f3ae8741b6faaef438ec35228e469f2bdc659e2
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-04-03 19:18:56 +0530

    Test: Add "Association" tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,9 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  it { is_expected.to(have_many(:followers).through(:relationships)) }
-  it { is_expected.to(have_many(:following).through(:relationships)) }
+  it { is_expected.to(have_many(:active_relations).with_foreign_key("follower_id").class_name("Relationship")) }
+  it { is_expected.to(have_many(:following).through(:active_relations).source(:followee)) }
+
+  it { is_expected.to(have_many(:passive_relations).with_foreign_key("followee_id").class_name("Relationship")) }
+  it { is_expected.to(have_many(:followers).through(:passive_relations).source(:follower)) }
 end
