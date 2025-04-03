6c6dc9a44d1507dcb34821265db61bd8b8521cdf
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-04-03 20:03:10 +0530

    DB: Add "unique" constraint for table

--- db/migrate/20250403141800_create_profiles.rb
+++ db/migrate/20250403141800_create_profiles.rb
@@ -1,12 +1,14 @@
 class CreateProfiles < ActiveRecord::Migration[8.0]
   def change
-    create_table :profiles do |t|
-      t.string :username
-      t.string :name
-      t.text :bio
-      t.references :user, null: false, foreign_key: true
+    create_table(:profiles) do |t|
+      t.string(:username)
+      t.string(:name)
+      t.text(:bio)
+      t.references(:user, null: false, foreign_key: true)
 
       t.timestamps
     end
+
+    add_index(:profiles, :username, unique: true)
   end
 end
