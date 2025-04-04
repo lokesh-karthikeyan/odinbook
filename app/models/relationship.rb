ec4845df98591c1d5881a65b8a6231b04967ef32
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-04-04 09:57:12 +0530

    DB: Add "status" column -> relationships

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,6 @@
 class Relationship < ApplicationRecord
   belongs_to :follower, class_name: "User"
   belongs_to :followee, class_name: "User"
+
+  enum status: { not_connected: 0, pending: 1, accepted: 2, rejected: 3 }
 end
