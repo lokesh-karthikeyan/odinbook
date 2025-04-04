af97e3c45284a2ba4291398c67c0f11a00926da2
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-04-04 14:19:29 +0530

    Model: Fix unnecessary "Hash" type for enum

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,5 +2,5 @@ class Relationship < ApplicationRecord
   belongs_to :follower, class_name: "User"
   belongs_to :followee, class_name: "User"
 
-  enum status: { not_connected: 0, pending: 1, accepted: 2, rejected: 3 }
+  enum :status, not_connected: 0, pending: 1, accepted: 2, rejected: 3
 end
