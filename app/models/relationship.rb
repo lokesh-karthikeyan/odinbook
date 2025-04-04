83e8365a6b9479ec278ce8febbf88d0b5b090ba8
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-04-04 20:41:25 +0530

    Model: Add "requested" key to enum_status

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,5 +2,5 @@ class Relationship < ApplicationRecord
   belongs_to :follower, class_name: "User"
   belongs_to :followee, class_name: "User"
 
-  enum :status, not_connected: 0, pending: 1, accepted: 2, rejected: 3
+  enum :status, not_connected: 0, requested: 1, pending: 2, accepted: 3, rejected: 4
 end
