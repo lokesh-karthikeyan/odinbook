a626f1a14b611460cf32bb3c8f2f955874f7fbad
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-04-04 20:38:48 +0530

    Controller: Add `#create` action controller

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,24 @@
 class RelationshipsController < ApplicationController
+  def create
+    other_user = User.find(relationship_params[:other_user])
+    Relationship.create(follower_id: current_user.id, followee_id: other_user.id, status: :requested)
+    Relationship.create(follower_id: other_user.id, followee_id: current_user.id, status: :pending)
+
+    respond_to do |format|
+      format.turbo_stream {
+        render(
+          turbo_stream: turbo_stream.replace(
+            "follow_link_#{other_user.id}",
+            partial: "users/links/requested",
+            locals: { other_user: other_user }
+          )
+        )
+        flash.now[:notice] = "Request has been successfully sent to #{other_user.profile.name}"
+      }
+    end
+  end
+
+  private
+
+  def relationship_params = params.permit(:other_user)
 end
