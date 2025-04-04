8f03476a05a0495771fb384efade99b265c5c0b7
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-04-04 14:21:11 +0530

    Feat: Add `#show_blob` to render images/svgs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,39 @@
 module ApplicationHelper
+  def show_blob(blob, resize_to: nil)
+    return unless blob.attached?
+
+    if blob.content_type == "image/svg+xml"
+      render_svg(blob, resize_to)
+    else
+      render_non_svg(blob, resize_to)
+    end
+  end
+
+  private
+
+  def render_svg(blob, resize_to)
+    svg_content = blob.download
+    return raw(svg_content) unless resize_to
+
+    svg_content.gsub!(/<svg[^>]+>/) do |svg_tag|
+      width, height = resize_to.downcase.split("x")
+      if svg_tag.include?("width") && svg_tag.include?("height")
+        svg_tag
+          .gsub(/width="[^"]+"/, "width=\"#{width}\"")
+          .gsub(/height="[^"]+"/, "height=\"#{height}\"")
+      else
+        svg_tag.sub("<svg", "<svg width=\"#{width}\" height=\"#{height}\"")
+      end
+    end
+
+    raw(svg_content)
+  end
+
+  def render_non_svg(blob, resize_to)
+    if resize_to && blob.content_type.match(/^image\/(jpeg|jpg|png)$/)
+      image_tag(blob.variant(resize_to: resize_to).processed, alt: "Avatar")
+    else
+      image_tag(url_for(blob), alt: "Avatar")
+    end
+  end
 end
