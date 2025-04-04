0e23004a102f9cc783db12a01d6cfc0b54b2acee
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-04-04 09:35:24 +0530

    Service: Add feature to generate avatars

---
+++ app/services/avatar_generator.rb
@@ -0,0 +1,22 @@
+class AvatarGenerator
+  def self.create_initials_avatar(initials)
+    svg = <<~SVG
+      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50">
+        <style>
+          @font-face {
+          font-family: "OpenSans";
+          font-weight: 500;
+          src: url(data:application/x-font-ttf;base64,<%= ::AvatarsController::ENCODED_FONT %>);
+          }
+        </style>
+        <rect width="100%" height="100%" fill="#{random_color}"/>
+        <text fill="#fff" font-family="OpenSans,Arial,sans-serif" font-size="26" font-weight="500" x="50%" y="55%" dominant-baseline="middle" text-anchor="middle">
+          #{initials}
+        </text>
+      </svg>
+    SVG
+    svg.strip
+  end
+
+  def self.random_color = ([ "#A9294F", "#ED6663", "#389393", "#D82148", "#8C5425", "#6F38C5" ].sample)
+end
