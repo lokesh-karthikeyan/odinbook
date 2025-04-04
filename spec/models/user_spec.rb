c031bfacb4b3b01561b5ffc9344043455a9384fa
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-04-04 20:54:06 +0530

    Test: Add "Association tests" to User <-> Profile

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,23 @@ RSpec.describe User, type: :model do
 
   it { is_expected.to(have_many(:passive_relations).with_foreign_key("followee_id").class_name("Relationship")) }
   it { is_expected.to(have_many(:followers).through(:passive_relations).source(:follower)) }
+
+  it { is_expected.to(have_one(:profile)) }
+
+  context("When the user is registering") do
+    it "should generate an username" do
+      user = create(:user)
+      expect(user.profile.username).to(be_present)
+    end
+
+    it "should generate an avatar" do
+      user = create(:user)
+      expect(user.profile.avatar.attached?).to(be_present)
+    end
+
+    it "should generate a name" do
+      user = create(:user)
+      expect(user.profile.name).to(be_present)
+    end
+  end
 end
