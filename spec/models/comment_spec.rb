05bc4d010e845877f6888547f541abd92e99967e
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-04-05 14:10:53 +0530

    Test: Add association tests - Post <-> Comment <-> Like

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,6 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to(belong_to(:post)) }
+  it { is_expected.to(belong_to(:user)) }
 end
