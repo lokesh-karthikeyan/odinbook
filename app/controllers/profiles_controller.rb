67586eb7b0330290761b71cad77f0b78f6457270
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-04-05 11:13:37 +0530

    Controller: Add `#show, #edit, #update` actions

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,29 @@
 class ProfilesController < ApplicationController
+  def show
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    if current_user
+        .profile
+        .update(
+          name: profile_params[:name],
+          username: profile_params[:username],
+          bio: profile_params[:bio],
+          avatar: profile_params[:avatar]
+        )
+      flash[:message] = "Profile updated successfully!"
+      redirect_to(user_profile_path(current_user))
+    else
+      flash.now[:alert] = "An Error occured!"
+      render(:edit, status: :unprocessable_entity)
+    end
+  end
+
+  private
+
+  def profile_params = params.expect(profile: [ :avatar, :name, :username, :bio ])
 end
