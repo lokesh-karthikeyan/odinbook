c733603122cf1ff03864be294771647a439381bf
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-04-06 10:00:33 +0530

    Controller: Add `#create, #new, #feed, #index` actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,34 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.includes(user: :profile)
+  end
+
+  def feed
+    @posts = current_user.posts
+  end
+
+  def new
+    @post = current_user.posts.build
+    respond_to do |format|
+      format.turbo_stream
+      format.html
+    end
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      respond_to do |format|
+        format.html { redirect_to(homepage_path, notice: "Post was successfully created") }
+        format.turbo_stream
+      end
+    else
+      render(:new, status: :unprocessable_entity)
+    end
+  end
+
+  private
+
+  def post_params
+  end
 end
