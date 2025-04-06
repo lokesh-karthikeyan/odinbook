fbf97784c4f86d42a11553acd522a7848c09722a
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-04-06 23:18:48 +0530

    Controller: Add `#new/#create` action controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,31 @@
 class CommentsController < ApplicationController
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to(:authenticated_homepage) }
+    end
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+
+    @comment.user_id = current_user.id
+
+    if @comment.save
+      respond_to do |format|
+        format.html { redirect_to(authenticated_homepage_path, notice: "Comment has been successfully submitted") }
+        format.turbo_stream
+      end
+    else
+      render(:new, status: :unprocessable_entity)
+    end
+  end
+
+  private
+
+  def comment_params = (params.expect(comment: [ :content ]))
 end
