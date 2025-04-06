7a8af187477b6ccb4abd1cbc0326cbcf1b27004b
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-04-07 01:35:58 +0530

    Controller: Add `#create/#destroy` actions

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,27 @@
 class LikesController < ApplicationController
+  before_action :set_post
+
+  def create
+    @like = @post.likes.new(user: current_user)
+
+    if @like.save
+      render(json: { like_count: @post.likes.count })
+    else
+      render(json: { error: "Unable to like post" }, status: :unprocessable_entity)
+    end
+  end
+
+  def destroy
+    @like = @post.likes.find_by(user: current_user)
+
+    if @like && @like.destroy
+      render(json: { like_count: @post.likes.count })
+    else
+      render(json: { error: "Unable to unlike post" }, status: :unprocessable_entity)
+    end
+  end
+
+  private
+
+  def set_post = (@post = Post.find(params[:id]))
 end
