3e68fee5572851f3fe934c83e7b9cc2d8f2ab173
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-04-07 17:14:16 +0530

    Controller: Add `#posts/#followers/#following` actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,33 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @posts = @user.posts
+  end
+
+  def posts
+    @user = User.find(params[:id])
+    @posts = @user.posts
+
+    respond_to do |format|
+      format.turbo_stream
+    end
+  end
+
+  def following
+    @user = User.find(params[:id])
+    @following = @user.following
+
+    respond_to do |format|
+      format.turbo_stream
+    end
+  end
+
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers
+
+    respond_to do |format|
+      format.turbo_stream
+    end
   end
 end
