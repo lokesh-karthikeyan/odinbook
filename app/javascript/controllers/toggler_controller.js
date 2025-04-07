bfadc75dbedf090f59cb7899398454a87cf5371c
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-04-07 21:07:09 +0530

    Stimulus: Add `Toggler.changeClass()` controller

--- app/javascript/controllers/toggler_controller.js
+++ app/javascript/controllers/toggler_controller.js
@@ -11,4 +11,18 @@ export default class extends Controller {
     this.element.classList.toggle(this.toggleClass);
     menuItem.classList.toggle(this.toggleClass);
   }
+
+  changeClass(event) {
+    let activeClass = "is-active";
+    let selectedItem = event.target.parentElement;
+    let menuTabs = document.querySelectorAll(".menu-tabs");
+
+    menuTabs.forEach((element) => {
+      if (element !== selectedItem) {
+        element.classList.remove(activeClass);
+      } else {
+        element.classList.add(activeClass);
+      }
+    });
+  }
 }
