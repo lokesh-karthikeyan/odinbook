08b0637eea8381e734832c61efeb5e3d9ab1095e
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-04-07 23:42:16 +0530

    Controller: Add conditionals based on context

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -24,25 +24,46 @@ class RelationshipsController < ApplicationController
   def show = (@users = current_user.pending_requests)
 
   def update
+    context = relationship_params[:context]
     relationship = Relationship.find_by(follower_id: relationship_params[:other_user], followee_id: current_user.id)
     other_user = User.find(relationship_params[:other_user])
 
     if relationship_params[:status] == "accepted"
       relationship.update(status: :accepted)
       flash[:notice] = "#{relationship.follower.profile.name} is now following you!"
-      render(turbo_stream: turbo_stream.replace("new_request_#{other_user.id}", ""))
+      if context.eql?("relationship_page")
+        render(turbo_stream: turbo_stream.replace("new_request_#{other_user.id}", ""))
+      elsif context.eql?("user_profile_page")
+        render(
+          turbo_stream: turbo_stream.replace(
+            "relations_status",
+            partial: "users/links/accepted",
+            locals: { user: other_user }
+          )
+        )
+      end
     elsif relationship_params[:status] == "rejected"
       relationship.update(status: :rejected)
       flash[:notice] = "Follow request rejected."
-      render(turbo_stream: turbo_stream.replace("new_request_#{other_user.id}", ""))
+      if context.eql?("relationship_page")
+        render(turbo_stream: turbo_stream.replace("new_request_#{other_user.id}", ""))
+      elsif context.eql?("user_profile_page")
+        render(
+          turbo_stream: turbo_stream.replace(
+            "relations_status",
+            partial: "users/links/rejected",
+            locals: { user: other_user }
+          )
+        )
+      end
     else
       redirect_to(relationship_path, alert: "Invalid status update.")
     end
   end
 
   def destroy
-    relationship = Relationship.find_by(follower_id: current_user.id, followee_id: relationship_params[:other_user])
-    other_user = User.find(relationship_params[:other_user])
+    other_user = User.find(params[:id])
+    relationship = Relationship.between_users(current_user, other_user).first
 
     if relationship
       relationship.destroy
@@ -50,7 +71,7 @@ class RelationshipsController < ApplicationController
       render(
         turbo_stream: turbo_stream.replace(
           "follow_link_#{other_user.id}",
-          partial: "users/links/rejected",
+          partial: "users/links/follow",
           locals: { other_user: other_user }
         )
       )
@@ -62,6 +83,6 @@ class RelationshipsController < ApplicationController
   private
 
   def relationship_params
-    params.permit(:other_user, :status, :id, :_method)
+    params.permit(:other_user, :status, :id, :_method, :context)
   end
 end
