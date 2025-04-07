3e39e51132946454ff760fd06eba1456f1032073
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-04-08 01:23:16 +0530

    Model: Add "Associations" User <-> Comment <-> Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :media
 
-  has_many :comments
-  has_many :likes
+  has_many :comments, dependent: :destroy
+  has_many :commenters, through: :comments, source: :user
+
+  has_many :likes, dependent: :destroy
 end
