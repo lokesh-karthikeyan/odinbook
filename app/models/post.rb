e118788cc095eaab502191001e90bc284eb11713
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-04-08 12:56:38 +0530

    Model: Add "Validations" for user inputs

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,21 @@ class Post < ApplicationRecord
   has_many :commenters, through: :comments, source: :user
 
   has_many :likes, dependent: :destroy
+
+  validate :media_format
+  validate :content_or_media_presence
+
+  private
+
+  def media_format
+    if media.attached? && !media.content_type.in?(%w[image/jpeg image/jpg image/png image/svg+xml])
+      errors.add(:media, "must be a PNG, JPEG, JPG, or SVG file")
+    end
+  end
+
+  def content_or_media_presence
+    if content.blank? && !media.attached?
+      errors.add(:base, "Post must have either content or media attached")
+    end
+  end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,16 @@
 class Profile < ApplicationRecord
   belongs_to :user, inverse_of: :profile
   has_one_attached :avatar
+
+  validates :name, presence: true, length: { minimum: 4, maximum: 30 }
+  validates :username, presence: true, length: { minimum: 3, maximum: 15 }, uniqueness: true
+  validate :avatar_format
+
+  private
+
+  def avatar_format
+    if avatar.attached? && !avatar.content_type.in?(%w[image/png image/jpeg image/jpg image/svg+xml])
+      errors.add(:avatar, "must be a PNG, JPG, JPEG or SVG file")
+    end
+  end
 end
