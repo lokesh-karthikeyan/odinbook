afa4d7fdd321a6ac876c0997f4a074851bc5e643
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-04-08 18:57:15 +0530

    Mailer: Add `#welcome` action mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,7 @@
 class UserMailer < ApplicationMailer
+  def welcome
+    @user = params[:user]
+    @url = "http://localhost:3000/login"
+    mail(to: @user.email, subject: "Welcome to OdinBook")
+  end
 end
